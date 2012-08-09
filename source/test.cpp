ed87072d521657cdb4f8d94d26816f3528074dde
andrey <andrey@andrey-K70AB>
2012-08-09 08:44:29 +0400

    zmqtest

--- source/test.cpp
+++ source/test.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+
+class DumpListenerClass
+{
+private:
+	unsigned int itsPort;
+
+public:
+	DumpListenerClass(unsigned int initialPort);
+	~DumpListenerClass();
+
+	unsigned int getPort() const
+	{
+		return itsPort;
+	}
+
+	void setPort(unsigned int port)
+	{
+		itsPort = port;
+	}
+
+};
+
+DumpListenerClass::DumpListenerClass(unsigned int initialPort)
+{
+	itsPort = initialPort;
+}
+
+DumpListenerClass::~DumpListenerClass()
+{
+}
+
+
 int main(int argc, char** argv)
 {
-	std::cout << "Hello, World!" << std::endl;
+	DumpListenerClass dumpListener(5555);
+	//dumpListener.setPort(6666);
+
+
+
+	std::cout << "Hello, World!  " << dumpListener.getPort() << std::endl;
 }
 
