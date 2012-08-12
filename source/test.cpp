8cd5a7ed609db730fdb5f53c9584d58d8fc998bd
andrey <andrey@andrey-K70AB>
2012-08-12 23:24:49 +0400

    coreClass. beginning

--- source/test.cpp
+++ source/test.cpp
@@ -1,43 +1,259 @@
+// my second program in C++
+
 #include <iostream>
+//#include <string>
+//using namespace std;
+
+//����� ����
+class CoreClass
+{
+private:
+	//��������� listeners[] subSystems[] clients[] ;
+
+	int logger;
+	int	config; //��������� ��� ������ ar['max_file_size'] = 7
+public:
+	//
+	CoreClass ()
+	{};
+	~CoreClass()
+	{};
+
+	void loadConfig ()
+	{
+		std::cout << "loadConfig\n";
+	};
+	void initLogging()// ����. ������������
+	{
+		std::cout << "initLogging\n";
+	};
+	void initSubSystems()
+	{
+		std::cout << "initSubSystems\n";
+	};
+
+
+	void initListeners()
+	{
+		std::cout << "initListeners\n";
+	};
+	void linkClients()
+	{
+		std::cout << "linkClients\n";
+	}; // ��������
+	void linkListeners()
+	{
+		std::cout << "linkListeners\n";
+	};
+	void linkSubSystems()
+	{
+		std::cout << "linkSubSystems\n";
+	};
+	void startListeners()
+	{
+		std::cout << "startListeners\n";
+	};
+
+
+};
+
+//������ ��������� ---------------------------
+class STrafficManagerClass
+{
+public:
+	//
+	STrafficManagerClass ()
+	{};
+	~STrafficManagerClass()
+	{};
+
+	void someStreetMagicHere ()
+	{
+		std::cout << "someStreetMagicHere\n";
+	};
+};
+
+
+class SAutorizationClass
+{
+private:
+	int sessionLifeTime;
+	char sessions[10];
+
+public:
+	//
+	SAutorizationClass ()
+	{};
+	~SAutorizationClass()
+	{};
+
+	void createSession ()
+	{
+		std::cout << "createSession\n";
+	};
+
+	bool sessionValidate (int dump) //�� ���
+	{
+		std::cout << "sessionValidate\n";
+		return false;
+	};
+};
+
+class STrafficSaverClass
+{
+private:
+
+public:
+	//
+	STrafficSaverClass ()
+	{};
+	~STrafficSaverClass ()
+	{};
+};
+
+class SLogingClass
+{
+private:
+
+public:
+	//
+	SLogingClass ()
+	{};
+	~SLogingClass ()
+	{};
+};
+
+
+//������ ���������� ---------------------------
+class DumpClass
+{
+private:
+	char sessionHash[5];
+	char sessionKey[5];
+	int dump;
+public:
+	//
+	DumpClass ()
+	{};
+	~DumpClass ()
+	{};
+
+	void parse ()
+	{
+		std::cout << "parse\n";
+	};
+	void unpack ()
+	{
+		std::cout << "unpack\n";
+	};
+	void toLittleEndian ()
+	{
+		std::cout << "toLittleEndian\n";
+	};
+};
+
+
+
+class ReceiverClass
+{
+private:
+	DumpClass Message;
+public:
+	//
+	ReceiverClass ()
+	{};
+	~ReceiverClass ()
+	{};
+	void toSTrafficManager ()
+	{
+		std::cout << "toSTrafficManager\n";
+	};
+
+};
 
 class DumpListenerClass
 {
 private:
-	unsigned int itsPort;
-
+	int port;
+	ReceiverClass recievers;//��������
 public:
-	DumpListenerClass(unsigned int initialPort);
-	~DumpListenerClass();
+	//
+	DumpListenerClass ()
+	{};
+	~DumpListenerClass ()
+	{};
 
-	unsigned int getPort() const
+	void startListen ()
 	{
-		return itsPort;
-	}
+		std::cout << "startListen\n";
+	};
 
-	void setPort(unsigned int port)
+	void stopListen ()
 	{
-		itsPort = port;
-	}
+		std::cout << "stopListen\n";
+	};
+
+	void getActiveSensorsCount ()
+	{
+		std::cout << "getActiveSensorsCount\n";
+	};
+
+	void manageReceiver ()
+	{
+		std::cout << "manageReceiver\n";
+	};
+};
+
+class SensorListenerClass
+{
+private:
+	int port;
+	char message[5];
+public:
+	//
+	SensorListenerClass ()
+	{};
+	~SensorListenerClass ()
+	{};
+	void startListen ()
+	{
+		std::cout << "startListen\n";
+	};
+
+	void stopListen ()
+	{
+		std::cout << "stopListen\n";
+	};
+
+	void receiveMessage ()
+	{
+		std::cout << "receiveMessage\n";
+	};
+
+	void sendMessage ()
+	{
+		std::cout << "sendMessage\n";
+	};
 
 };
 
-DumpListenerClass::DumpListenerClass(unsigned int initialPort)
+int main ()
 {
-	itsPort = initialPort;
-}
-
-DumpListenerClass::~DumpListenerClass()
-{
-}
-
-
-int main(int argc, char** argv)
-{
-	DumpListenerClass dumpListener(5555);
-	//dumpListener.setPort(6666);
+	CoreClass Core;
+	Core.loadConfig();
+	Core.initLogging();
+	Core.initSubSystems();
+	///�������
+	Core.linkClients();
+	Core.initListeners();
+	Core.linkListeners();
+	Core.startListeners();
 
 
-
-	std::cout << "Hello, World!  " << dumpListener.getPort() << std::endl;
+	int i=5;
+	//std::cout << i;
+	std::cin >>i;
+	system("pause");/////////////
+	return 0;
 }
 
